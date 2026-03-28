d4e03d8f6dad3f04da28d97a6fd76f3cbf681930
KunalAyare <ayarekunal062@gmail.com>
2026-03-28 18:08:18 +0530

    Configured retries for CI and local

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,10 +1,14 @@
 import {test, expect} from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 
-test('login to saucedemo', async ({ page }) => {
+test.describe('Login tests', () => {
+
+test.only('login to saucedemo', async ({ page }) => {
     
     const loginPage = new LoginPage(page);
     await loginPage.goto();
     await loginPage.login('standard_user', 'secret_sauce');
     await expect(page).toHaveURL(/inventory/);
     });
+
+});
